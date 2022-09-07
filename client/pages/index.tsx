cd947cfbcd4b96dfa924bc11529a6c768557e391
Bas <BasanM@MacBook.Dlink>
2022-09-07 23:09:02 +0300

    addTrackComponent

---
+++ client/pages/index.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/no-unknown-property */
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Index = () => {
+    return (
+        <>
+        <Navbar/>
+            <div className='center'>
+             <h1>Добро пожаловать!</h1>
+             <h3>Здесь собраны лучшие треки</h3>
+            </div>
+            <style jsx>
+                {
+                    `
+                    .center{
+                        margin-top: 150px;
+                        display: flex;
+                        flex-direction: column;
+                        align-items: center;
+                        justify-content: center;
+                    }
+                    `
+                }
+            </style>
+        </>
+    )
+}
+export default Index
